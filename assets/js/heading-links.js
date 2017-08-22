2e8aa91714edd8425a829a62aa9a10a884f15c93
Maya Benari <hello@maya-benari.com>
2017-08-22 10:23:11 -0700

    Add heading anchor links

---
+++ assets/js/heading-links.js
@@ -0,0 +1,31 @@
+var anchorForId = function (id) {
+  var anchor = document.createElement("a");
+  anchor.className = "header-link";
+  anchor.href      = "#" + id;
+  anchor.innerHTML = "<span class=\"usa-sr-only\">Permalink</span><i class=\"fa fa-link\"></i>";
+  anchor.title = "Permalink";
+  return anchor;
+};
+
+var linkifyAnchors = function (level, containingElement) {
+  var headers = containingElement.getElementsByTagName("h" + level);
+  for (var h = 0; h < headers.length; h++) {
+    var header = headers[h];
+
+    if (typeof header.id !== "undefined" && header.id !== "") {
+      header.appendChild(anchorForId(header.id));
+    }
+  }
+};
+
+document.onreadystatechange = function () {
+  if (this.readyState === "complete") {
+    var contentBlock = document.getElementsByClassName("usa-layout-docs")[0] || document.getElementsByClassName("news")[0];
+    if (!contentBlock) {
+      return;
+    }
+    for (var level = 1; level <= 6; level++) {
+      linkifyAnchors(level, contentBlock);
+    }
+  }
+};
